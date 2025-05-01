253fb5c418823ffb7b8833bf626e04e53ffce301
Kumaran S <kumaran.s@velammal.edu.in>
2025-05-02 00:24:33 +0530

    working as expected

--- index.js
+++ index.js
@@ -32,23 +32,23 @@ function newNote() {
 function deleteAllNotes() {
     if(localStorage){
         localStorage.clear()
-        savedNotes = []
+        toast("Deleted all Notes")
+        renderNotes()
     }
-    toast("Deleted all Notes")
-    renderNotes()
 }
 
 function saveNotes() {
-    const title = document.getElementById("title")
+    const noteTitle = document.getElementById("title")
     const notes = document.getElementById("Note")
     let Notes = JSON.parse( localStorage.getItem("myNotes") ) || []
-    let newNotes = []
-    if (title.value && notes.value) {
+    if (noteTitle.value && notes.value) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            newNotes.push(tabs[0].favIconUrl, tabs[0].title, tabs[0].url, title.value, notes.value) 
-            Notes.push(newNotes)
+            console.log(tabs)
+            console.log(tabs[0])
+            const url = new URL(tabs[0].url)
+            Notes.push([tabs[0].favIconUrl, url.hostname, tabs[0].url, noteTitle.value, notes.value])
             localStorage.setItem("myNotes", JSON.stringify(Notes))
-            title.value = ''
+            noteTitle.value = ''
             notes.value = ''
         })
         homePopup.style.display = 'block'
@@ -57,7 +57,7 @@ function saveNotes() {
         renderNotes()
         }
     else {
-        if (title.value) {
+        if (noteTitle.value) {
             toast("Notes can't be blank")
         }
         else {
@@ -70,7 +70,7 @@ function toast(message) {
     var x = document.getElementById("toast");
     x.innerText = message
     x.className = "show";
-    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000)
 }
 
 function quitNoteMaking() {
@@ -80,45 +80,78 @@ function quitNoteMaking() {
     newNotePopup.style.display = 'none'
 }
 
-function renderNotes() {
-    const savedNotes = JSON.parse(localStorage.getItem("myNotes")) || [];
-    const notesList = document.getElementById("noteList");
-    notesList.innerHTML = "";
+async function renderNotes() {
+    const savedNotes = JSON.parse(localStorage.getItem("myNotes")) || []
+    const notesList = document.getElementById("noteList")
+    notesList.innerHTML = ""
     /*
-    const headerLi = document.createElement("li");
-    const headerSpan = document.createElement("span");
-    const titleHeader = document.createElement("p");
-    const siteHeader = document.createElement("p");
-    const notesHeader = document.createElement("p");
-    headerSpan.id = "titleTag";
-    titleHeader.textContent = "Title";
-    siteHeader.textContent = "Site";
-    notesHeader.textContent = "Notes";
-    headerSpan.appendChild(titleHeader);
-    headerSpan.appendChild(siteHeader);
-    headerSpan.appendChild(notesHeader);
-    headerLi.appendChild(headerSpan);
-    notesList.appendChild(headerLi);*/
+    const headerLi = document.createElement("li")
+    const headerSpan = document.createElement("span")
+    const titleHeader = document.createElement("p")
+    const siteHeader = document.createElement("p")
+    const notesHeader = document.createElement("p")
+    headerSpan.id = "titleTag"
+    titleHeader.textContent = "Title"
+    siteHeader.textContent = "Site"
+    notesHeader.textContent = "Notes"
+    headerSpan.appendChild(titleHeader)
+    headerSpan.appendChild(siteHeader)
+    headerSpan.appendChild(notesHeader)
+    headerLi.appendChild(headerSpan)
+    notesList.appendChild(headerLi)
+    */
 
     for (let i = 0; i < savedNotes.length; i++) {
-        const [title, siteText, note, url] = savedNotes[i];
-        const li = document.createElement("li");
-        const span = document.createElement("span");
-        const titleEl = document.createElement("p");
-        const siteEl = document.createElement("p");
-        const link = document.createElement("a");
-        const noteEl = document.createElement("p");
-        titleEl.textContent = title;
-        link.href = url;
-        link.target = "_blank";
-        link.textContent = siteText;
-        siteEl.appendChild(link);
-        noteEl.textContent = note;
-        span.appendChild(titleEl);
-        span.appendChild(siteEl);
-        span.appendChild(noteEl);
-        li.appendChild(span);
-        notesList.appendChild(li);
+        const [favIconUrl, domain, url, title, note] = savedNotes[i]
+        
+        // HTML tags creation
+        const li = document.createElement("li")
+        const span = document.createElement("span")
+        const img = document.createElement("img")
+        const domainName = document.createElement("p")
+        const noteTitle = document.createElement("p")
+        const link = document.createElement("a")
+        const notes = document.createElement("p")
+
+        // img
+        img.src = favIconUrl
+        img.alt = "Site icon"
+
+        // link
+        link.href = url
+        link.target = "_blank"
+        link.textContent = domain
+
+        // p tag items
+        noteTitle.textContent = title
+        notes.textContent = note
+        
+        // appending tags
+        /* 
+        0-> li
+        1-> span
+        2-> img
+        3-> domain
+        4-> noteTitle
+        5-> notes
+
+        example:
+        <li>
+            <span>
+                <img src=${favIconUrl} alr = "Site icon"/>
+                <p id="domain"><a id = "link" href= ${url} target="_blank">${domainName}</a></p>
+                <p id="noteTtile">${title}</p>
+                <p id ="notes">${note}</p>
+            </span>
+        </li>
+        */
+        domainName.appendChild(link)
+        span.appendChild(img)
+        span.appendChild(domainName)
+        span.appendChild(noteTitle)
+        span.appendChild(notes)
+        li.appendChild(span)
+        notesList.appendChild(li)
     }
     
     /* 
